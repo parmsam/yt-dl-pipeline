2bafe82964e99e5bc03877d77a741d3253fde22f
Sam Parmar <40672605+parmsam@users.noreply.github.com>
2024-02-02 14:35:19 -0500

    Delete download_videos.sh

--- download_videos.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# include message of what is going on
-
-# download the data from the sources as mp4 files
-echo "Downloading the data from the sources as mp4 files"
-yt-dlp -a video_links.txt -f mp4
